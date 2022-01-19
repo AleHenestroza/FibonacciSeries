5c4c34ff58d0da701d2e07f2641cdf93f628bde3
Alejandro Henestroza <alehenestroza@gmail.com>
2022-01-18 22:31:28 -0300

    Basic implementation of the Fibonacci algorithm

--- src/ar/com/alehenestroza/Main.java
+++ src/ar/com/alehenestroza/Main.java
@@ -1,9 +1,23 @@
 package ar.com.alehenestroza;
 
+import java.util.Scanner;
+
 public class Main {
 
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // your code goes here
+        System.out.print("Enter a number: ");
+        int n = in.nextInt();
+
+        long fib = fibonacci(n);
+    }
+
+    private static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
 }
